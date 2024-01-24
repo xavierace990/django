1a2e42bed204d11e2e54fc381f675d4917ef1520
xavierace990 <141823546+xavierace990@users.noreply.github.com>
2024-01-25 00:28:07 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "make migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect static..."
+python3.9 manage.py collectstatic --noinput --clear
